f37cf9e781289ed03547766a42fc1e4eaac7fd21
Abhishek R Bhat <100010498+abhi61703@users.noreply.github.com>
2022-11-22 19:55:43 +0530

    Create Tom_and_Jerry.c

---
+++ Tom_and_Jerry.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include<math.h>
+int main(void) {
+	// your code goes here
+	int tc;
+	scanf("%d",&tc);
+	while(tc--){
+	    int a,b,c,d,k;
+	    scanf("%d%d%d%d%d",&a,&b,&c,&d,&k);
+    	int x=abs(a-c)+abs(b-d);
+        if(x>k)
+        {
+            printf("NO\n");
+        }
+        else if(x==k)
+            printf("YES\n");
+        else if((k-x)%2==0)
+            printf("YES\n");
+        else
+            printf("NO\n");
+	            
+	}
+    
+	return 0;
+}
+
