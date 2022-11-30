b99154e2a90a176e32e436d2ef861d8168f4ae22
Abhishek R Bhat <100010498+abhi61703@users.noreply.github.com>
2022-11-30 21:55:52 +0530

    Create Maximum_Angriness.c

---
+++ Maximum_Angriness.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int tc;
+	scanf("%d",&tc);
+	while(tc--){
+	    long long int N,K,sum;
+	    scanf("%lld%lld",&N,&K);
+	    sum=(N*(N-1))/2;
+	    if(K<N/2){
+	        long long int x=N-2*K;
+	        sum-=(x*(x-1))/2;
+	    }
+	    printf("%lld\n",sum);
+	}
+    
+	return 0;
+}
+
