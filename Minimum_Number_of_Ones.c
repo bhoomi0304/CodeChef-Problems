d5a56cc63023a97d0073501839168a0c32d529ab
Abhishek R Bhat <100010498+abhi61703@users.noreply.github.com>
2022-11-30 21:54:56 +0530

    Create Minimum_Number_of_Ones.c

---
+++ Minimum_Number_of_Ones.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int tc;
+	scanf("%d",&tc);
+	while(tc--){
+	    int min;
+	    scanf("%d",&min);
+	    printf("%d\n",(min/2));
+	}
+    
+	return 0;
+}
+
