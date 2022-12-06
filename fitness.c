5af01b0a024cbca57f419492a10ca79daadcdb57
Abhishek R Bhat <100010498+abhi61703@users.noreply.github.com>
2022-12-06 21:11:27 +0530

    Create fitness.c

---
+++ fitness.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int tc;
+	scanf("%d",&tc);
+	while(tc--){
+	    int km;
+	    scanf("%d",&km);
+	    printf("%d\n",(2*km)*5);
+	}
+    
+	return 0;
+}
+
