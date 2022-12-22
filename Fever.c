9d3e73d461dc97928e42ae01a29293edcceaaa76
Abhishek R Bhat <100010498+abhi61703@users.noreply.github.com>
2022-12-22 21:33:57 +0530

    Create Fever.c

---
+++ Fever.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int tc;
+	scanf("%d",&tc);
+	while(tc--){
+	    int t;
+	    scanf("%d",&t);
+	    (t>98)?printf("YES\n"):printf("NO\n");
+	}
+    
+	return 0;
+}
+
