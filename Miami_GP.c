b8547ae544846b4f664c2137bd47294b56174e13
Abhishek R Bhat <100010498+abhi61703@users.noreply.github.com>
2023-01-01 21:17:50 +0530

    Create Miami_GP.c

---
+++ Miami_GP.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int tc;
+	scanf("%d",&tc);
+	while(tc--){
+	   int x,y;
+	   scanf("%d%d",&x,&y);
+	   (x*1.07 >= y)?printf("Yes\n"):printf("No\n");
+	}
+	return 0;
+}
+
