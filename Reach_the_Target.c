d358f7f416050a7aa32bb1af82b8fbc60a8956cb
Abhishek R Bhat <100010498+abhi61703@users.noreply.github.com>
2023-01-05 21:15:48 +0530

    Create Reach_the_Target.c

---
+++ Reach_the_Target.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int tc;
+	scanf("%d",&tc);
+	while(tc--){
+	    int a,b;
+	    scanf("%d%d",&a,&b);
+	    printf("%d\n",a-b);
+	    
+	}
+	return 0;
+}
+
