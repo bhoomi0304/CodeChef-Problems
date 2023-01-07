8199d16767d863c165f93fabc0d146ffab159c5d
Abhishek R Bhat <100010498+abhi61703@users.noreply.github.com>
2023-01-07 22:24:26 +0530

    Create Economic_Class.c

---
+++ Economic_Class.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int tc;
+	scanf("%d",&tc);
+	while(tc--)
+	{
+	    int n;
+	    scanf("%d",&n);
+	    int s[n];
+	    int d[n];
+	    int i=0,count=0;
+	    for(i=0;i<n;i++)
+	    {
+	        scanf("%d",&s[i]);
+	    }
+	    for(i=0;i<n;i++)
+	    {
+	        scanf("%d",&d[i]);
+	    }
+	    for(i=0;i<n;i++)
+	    {
+	        if(s[i]==d[i])
+	        count++;
+	    }
+	    printf("%d\n",count);
+	}
+	return 0;
+}
