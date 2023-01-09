a2619c18566ea8d129c0c0fcf7e148d91e8262a4
Abhishek R Bhat <100010498+abhi61703@users.noreply.github.com>
2023-01-09 22:30:16 +0530

    Create Second_Max_of_Three_Numbers.c

---
+++ Second_Max_of_Three_Numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int tc;
+	scanf("%d",&tc);
+	while(tc--){
+	    int a,b,c;
+	    scanf(" %d%d%d",&a,&b,&c);
+        if((a>b||a>c)&&(a<c||a<b))
+            printf("%d\n",a);
+        else if((b>a||b>c)&&(b<c||b<a))
+            printf("%d\n",b);
+        else
+            printf("%d\n",c);
+	}
+	return 0;
+}
+
