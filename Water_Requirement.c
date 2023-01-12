9e5002d0d04de20ed06717fcac308c0d7aa215a6
Abhishek R Bhat <100010498+abhi61703@users.noreply.github.com>
2023-01-12 22:07:34 +0530

    Create Water_Requirement.c

---
+++ Water_Requirement.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int tc;
+	scanf("%d",&tc);
+	while(tc--){
+	   int hours;
+	   scanf("%d",&hours);
+	   printf("%d\n",2*hours);
+	}
+	return 0;
+}
+
