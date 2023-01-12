cf7e211fe0f55fa054f7bd1f2efdd5ebcd70a323
Abhishek R Bhat <100010498+abhi61703@users.noreply.github.com>
2023-01-12 22:07:12 +0530

    Create Chef_Plays_Ludo.c

---
+++ Chef_Plays_Ludo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int tc;
+	scanf("%d",&tc);
+	while(tc--){
+	    int n;
+	    scanf("%d",&n);
+	    if(n==6){
+	        printf("YES\n");
+	    }
+	    else{
+	        printf("NO\n");
+	    }
+	}
+	return 0;
+}
