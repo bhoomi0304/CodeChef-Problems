4abf7ef2e23ece89c5d6801cf6fcfbf53f8f4fbe
Abhishek R Bhat <100010498+abhi61703@users.noreply.github.com>
2023-01-14 22:36:55 +0530

    Create My_Very_First_Contest.c

---
+++ My_Very_First_Contest.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+ int main()
+
+ {
+     int N,A,B,f,s;
+
+     scanf("%d %d %d",&N,&A,&B);
+
+     f = N-A;
+
+     s = f-B;
+
+     printf("%d %d\n",f,s);
+     
+     return 0;
+
+
+ }
