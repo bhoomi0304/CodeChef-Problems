e4ff65da2a9facc6d20feed1682e8b8dd4457bff
Abhishek R Bhat <100010498+abhi61703@users.noreply.github.com>
2023-03-21 11:51:54 +0530

    Create Good_Turn.c

---
+++ Good_Turn.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int tc;
+	scanf("%d",&tc);
+	while(tc--){
+	    int x,y;
+	    scanf("%d%d",&x,&y);
+	    ((x+y)>6)?printf("YES\n"):printf("NO\n");
+	}
+	return 0;
+}
+
