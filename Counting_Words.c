050d57d1e116e3a2ca7689f7c1ec6e0f581a2ed6
Abhishek R Bhat <100010498+abhi61703@users.noreply.github.com>
2023-03-26 20:13:17 +0530

    Create Counting_Words.c

---
+++ Counting_Words.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int tc;
+	scanf("%d",&tc);
+	while(tc--){
+	    int n,k;
+	    scanf("%d%d",&n,&k);
+	    printf("%d\n",n*k);
+	}
+	return 0;
+}
+
