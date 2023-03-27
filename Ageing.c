d331e91dc6f7cccbc38a5270e8450ec62265361e
Abhishek R Bhat <100010498+abhi61703@users.noreply.github.com>
2023-03-27 10:35:47 +0530

    Create Ageing.c

---
+++ Ageing.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int tc;
+	scanf("%d",&tc);
+	while(tc--){
+	    int chef;
+	    scanf("%d",&chef);
+	    printf("%d\n",chef-10);
+	}
+	return 0;
+}
+
