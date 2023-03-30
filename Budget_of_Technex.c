eb3ad15a632e17575bce7ed66b7ceca1aff2281a
Abhishek R Bhat <100010498+abhi61703@users.noreply.github.com>
2023-03-30 21:29:00 +0530

    Create Budget_of_Technex.c

---
+++ Budget_of_Technex.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int tc;
+	scanf("%d",&tc);
+	while(tc--){
+	    int budge;
+	    scanf("%d",&budge);
+	    printf("%d\n",(budge*1000)/10);
+	}
+	return 0;
+}
+
