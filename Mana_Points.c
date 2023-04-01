a8a667eb6083dad4c774710dd900286d5c838b87
Abhishek R Bhat <100010498+abhi61703@users.noreply.github.com>
2023-04-01 22:18:05 +0530

    Create Mana_Points.c

---
+++ Mana_Points.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    int t,x,y;
+    scanf("%d",&t);
+    while(t--)
+    {
+        scanf("%d %d",&x,&y);
+        printf("%d\n",y/x);
+        
+    }
+	// your code goes here
+	return 0;
+}
