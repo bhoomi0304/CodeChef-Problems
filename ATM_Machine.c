2fc72f6a385fc81ec22261f278ff8b3bc5633abf
Abhishek R Bhat <100010498+abhi61703@users.noreply.github.com>
2023-04-07 21:43:10 +0530

    Create ATM_Machine.c

---
+++ ATM_Machine.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int tc;
+	scanf("%d",&tc);
+	while(tc--){
+	    int n,k;
+	    scanf("%d%d", &n, &k);
+	    int a[n];
+	    for(int i=0;i<n;i++){
+	        scanf("%d", &a[i]);
+	    }
+	    for(int i=0;i<n;i++){
+	        if((k-a[i])>=0){
+	            printf("1");
+	            k-=a[i];
+	        }
+	        else{
+	            printf("0");
+	        }
+	    }
+	    printf("\n");
+	}
+	return 0;
+}
+
